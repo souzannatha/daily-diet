52128724f090e6731ea954249de978e6b8c591c4
souzannatha <souzanatha04@gmail.com>
2026-03-08 16:45:32 -0300

    feat: create route summary  meals

--- src/routes/meals.ts
+++ src/routes/meals.ts
@@ -5,6 +5,7 @@ import { randomUUID } from 'node:crypto';
 import { checkSessionIdExists } from '../middlewares/check-session-id-exists';
 
 export function mealsRoutes(app: FastifyInstance) {
+
   app.post(
     '/',
     {
@@ -73,6 +74,46 @@ export function mealsRoutes(app: FastifyInstance) {
     return { meal };
   });
 
+  app.get('/summary', { preHandler: [checkSessionIdExists] }, async (request, reply) => {
+
+    const { sessionId } = request.cookies
+    const user = await knex('users').where('session_id', sessionId).first()
+
+    if (!user) {
+      return reply.status(400).send({ message: 'User Not Found' });
+    }
+
+
+    const meals = await knex('meals').where('user_id', user.id).select('*');
+    const totalMeals = meals.length
+
+    const mealsOnDiet = meals.filter((meal) => meal.is_on_diet)
+
+    const mealsOffDiet = meals.filter((meal) => !meal.is_on_diet)
+
+    let currentSequence = 0
+    let bestSequence = 0
+
+    for (const meal of meals) {
+      if (meal.is_on_diet) {
+        currentSequence++
+
+        if (currentSequence > bestSequence) {
+          bestSequence = currentSequence
+        }
+      } else {
+        currentSequence = 0
+      }
+    }
+
+    return reply.send({
+      totalMeals,
+      totalOnDiet: mealsOnDiet.length,
+      totalOffDiet: mealsOffDiet.length,
+      bestDietSequence: bestSequence
+    })
+  })
+
   app.put('/:id', { preHandler: checkSessionIdExists }, async (request, reply) => {
     const idMealSchema = z.object({
       id: z.uuid(),
@@ -111,19 +152,28 @@ export function mealsRoutes(app: FastifyInstance) {
   app.delete('/:id', { preHandler: checkSessionIdExists }, async (request, reply) => {
 
     const idMeal = z.object({
-      id: z.string(),
+      id: z.uuid(),
     });
 
     const { id } = idMeal.parse(request.params)
+    const { sessionId } = request.cookies;
 
-    const meal = await knex('meals').where('id').first()
+    const user = await knex('users').where('session_id', sessionId).first();
+
+    if (!user) {
+      return reply.status(401).send({ message: 'Unauthorized' });
+    }
+
+    const meal = await knex('meals').where({ id }).first()
 
     if (!meal) {
       return reply.status(404).send({ error: 'Meal not found' });
     }
 
-    await knex('meals').where({ id: id }).delete()
-    return reply.send(204).send()
+    await knex('meals').where({
+      id: id, user_id: user.id
+    }).delete()
+    return reply.status(204).send()
   });
 
 }
