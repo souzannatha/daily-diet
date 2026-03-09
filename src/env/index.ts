0695f201077a11083626cbad762871c0f2198841
souzannatha <souzanatha04@gmail.com>
2026-03-09 17:12:07 -0300

    chore: production config

--- src/env/index.ts
+++ src/env/index.ts
@@ -1,4 +1,4 @@
-import { z } from "zod";
+import { z } from 'zod'
 import { config } from 'dotenv'
 
 if (process.env.NODE_ENV === 'test') {
@@ -9,8 +9,9 @@ if (process.env.NODE_ENV === 'test') {
 
 const envSchema = z.object({
   NODE_ENV: z.enum(['development', 'test', 'production']).default('production'),
+  DATABASE_CLIENT: z.enum(['sqlite', 'pg']),
   DATABASE_URL: z.string(),
-  PORT: z.number().default(3333),
+  PORT: z.coerce.number().default(3333),
 })
 
 const _env = envSchema.safeParse(process.env)
